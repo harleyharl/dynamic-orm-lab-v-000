2862586e7cb5056ecf550513f68b7806a1c82227
Harley Rayner <harleymteden@gmail.com>
2018-11-10 01:43:29 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,12 +53,12 @@ class InteractiveRecord
   end
 
   def self.find_by(attributes={})
-    binding.pry
-    self.column_names.each do |column_name|
-      column_name == attributes.to_s
-    end
-    #returns a row
-    sql = "SELECT * FROM #{self.table_name} WHERE #{self.column_name}"
+    # binding.pry
+    # self.column_names.each do |column_name|
+    #   column_name == attributes.to_s
+    # end
+    # #returns a row
+    # sql = "SELECT * FROM #{self.table_name} WHERE #{self.column_name}"
 
     attributes.each do |attribute_name, attribute_value|
       binding.pry
